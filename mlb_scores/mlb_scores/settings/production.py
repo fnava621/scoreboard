b48614490d6aaa5661eacb724ed700bf5e4b26fa
Fernando Nava <fnava621@gmail.com>
2013-06-04 10:24:07 -0700

    fixed db

--- mlb_scores/mlb_scores/settings/production.py
+++ mlb_scores/mlb_scores/settings/production.py
@@ -7,21 +7,19 @@ import dj_database_url
 
 
 
-if environ.has_key('DATABASE_URL'):
-    url = urlparse(environ['DATABASE_URL'])
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': url.path[1:],
-        'USER': url.username,
-        'PASSWORD': url.password,
-        'HOST': url.hostname,
-        'PORT': url.port,
-    }
 
 
 
 
 
+
+
+
+
+
+
+
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
