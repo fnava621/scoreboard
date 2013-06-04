28c868b977951a8e23e99c29ea7d3cb150fb5642
Fernando Nava <fnava621@gmail.com>
2013-06-04 10:07:04 -0700

    heroku psql db

--- mlb_scores/mlb_scores/settings/production.py
+++ mlb_scores/mlb_scores/settings/production.py
@@ -1,15 +1,21 @@
 from .base import *
 from os import environ
-from django.core.exceptions import ImproperlyConfiguredfrom
+from django.core.exceptions import ImproperlyConfigured
 import dj_database_url
 
 
 
 
-
-
-
-
+if environ.has_key('DATABASE_URL'):
+    url = urlparse(environ['DATABASE_URL'])
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+    }
 
 
 
