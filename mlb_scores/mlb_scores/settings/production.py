9174049cd399386a123502f318e1cc7b2301a29b
Fernando Nava <fnava621@gmail.com>
2013-06-05 09:40:07 -0700

    fixe bug

--- mlb_scores/mlb_scores/settings/production.py
+++ mlb_scores/mlb_scores/settings/production.py
@@ -5,18 +5,18 @@ from django.core.exceptions import ImproperlyConfigured
 import dj_database_url
 
 
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
-
-
-
-
-
-
-
 DATABASES = {}
-DATABASES['default'] =  dj_database_url.config()
+
+SOUTH_DATABASE_ADAPTERS = {'default':'south.db.postgresql_psycopg2'}
+
+
+
+DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 
